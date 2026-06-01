903d68dfbb1951635fcc622f8d204f9e9d237b36
tmad4000 <dev.jacobcole@gmail.com>
2026-06-01 15:43:27 -0700

    chore(version): bump to 0.1.9 for build 41 — NVC composer + 84u.1 (sans associatedDomains)

--- app.config.js
+++ app.config.js
@@ -28,7 +28,7 @@ module.exports = {
   expo: {
     name: 'OpenChat',
     slug: 'openchat-mobile',
-    version: '0.1.8',
+    version: '0.1.9',
     orientation: 'portrait',
     icon: './assets/icon.png',
     scheme: 'openchat',
