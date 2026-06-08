a46603bc4e517b825d7b6b7ebe787138f1e48b7a
tmad4000 <dev.jacobcole@gmail.com>
2026-06-07 22:16:55 -0700

    chore(version): bump for local build at 22:16

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -28,7 +28,7 @@ module.exports = {
   expo: {
     name: 'OpenChat',
     slug: 'openchat-mobile',
-    version: '0.1.22',
+    version: '0.1.23',
     orientation: 'portrait',
     icon: './assets/icon.png',
     scheme: 'openchat',
